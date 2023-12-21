eb2f71784cefd12af2d9fe851d07b55834acd40a
phamtanphat <phatdroid94@gmail.com>
2023-12-21 20:53:09 +0700

    setup ui

--- app/src/main/java/com/example/lifecycleactivity11092023/MainActivity.kt
+++ app/src/main/java/com/example/lifecycleactivity11092023/MainActivity.kt
@@ -3,12 +3,21 @@ package com.example.lifecycleactivity11092023
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnNavigateMain2: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Log.d("pphat", "onCreate")
+
+        btnNavigateMain2 = findViewById(R.id.button_navigate_main2)
+
+        btnNavigateMain2.setOnClickListener {
+
+        }
     }
 
     override fun onStart() {
@@ -18,7 +27,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
-        Log.d("pphat", "onStart")
+        Log.d("pphat", "onResume")
     }
 
     override fun onPause() {
---
+++ app/src/main/java/com/example/lifecycleactivity11092023/MainActivity2.kt
@@ -0,0 +1,11 @@
+package com.example.lifecycleactivity11092023
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
